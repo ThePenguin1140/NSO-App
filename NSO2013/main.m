14a081fb460f40a040a43c30c0d17d7c78dcbe42
C2 <C2@C2s-MacBook-Air.local>
2013-11-04 18:30:17 -0400

    Initial Commit

---
+++ NSO2013/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NSO2013
+//
+//  Created by C2 on 2013-11-04.
+//  Copyright (c) 2013 C2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

dd25ea110f623a268be07d55715aa6ba56a87158
CS212 <cs212@Chris-Vesseys-Mac-mini-6.local>
2013-11-07 13:07:14 -0400

    missing commit

--- NSO-App/CampusInfoViewController.h
+++ NSO-App/CampusInfoViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface CampusInfoViewController : UIViewController
-
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
+- (IBAction)segmentControlAction:(id)sender;
 @end
--- NSO-App/CampusInfoViewController.m
+++ NSO-App/CampusInfoViewController.m
@@ -13,6 +13,7 @@
 @end
 
 @implementation CampusInfoViewController
+@synthesize webView;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -27,6 +28,15 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    //BE SURE TO LOAD THE FIRST HTML FILE INTO THE WEBVIEW
+    
+    //Implement path getting things where the path to html file is
+    // relative!
+    
+    NSURL *url = [NSURL URLWithString:@"/Users/cs212/Desktop/Project/NSO-App/webView1.html"];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    [webView loadRequest:urlRequest];
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,4 +45,21 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)segmentControlAction:(id)sender {
+    UISegmentedControl *segControl = (UISegmentedControl *)sender;
+    int selectedValue = segControl.selectedSegmentIndex;
+    switch (selectedValue) {
+        case 0:
+            NSLog(@"1");
+            break;
+        case 1:
+            NSLog(@"2");
+            break;
+        case 2:
+            NSLog(@"3");
+            break;
+        default:
+            break;
+    }
+}
 @end

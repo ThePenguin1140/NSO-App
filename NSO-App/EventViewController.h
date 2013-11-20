525f39b06fa70207bc4637b79428115d89f4e6cb
ThePenguin1140 <jwuertz@upei.ca>
2013-11-20 17:43:43 -0400

    Added Table View Controllers for Event Database

---
+++ NSO-App/EventViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventViewController.h
+//  NSO-App
+//
+//  Created by CS212 on 13-11-20.
+//  Copyright (c) 2013 Jason&Cory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventViewController : UITableViewController
+
+@end

8a6a5a6567bf20a5a3fcf5c671d8ab27364c09e4
ThePenguin1140 <jwuertz@upei.ca>
2013-11-20 13:33:48 -0400

    Added fith view
    Implemented five tabs, added MapViewController

---
+++ NSO-App/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  NSO-App
+//
+//  Created by CS212 on 13-11-20.
+//  Copyright (c) 2013 Jason&Cory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end

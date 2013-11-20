2982fdd9bc6f5adb7467ac6491021d2e295454a4
ThePenguin1140 <jwuertz@upei.ca>
2013-11-20 17:32:29 -0400

    Core data implementation
    fixed core data related crashes
    fully implemented the data base

---
+++ Day.m
@@ -0,0 +1,19 @@
+//
+//  Day.m
+//  NSO-App
+//
+//  Created by CS212 on 13-11-20.
+//  Copyright (c) 2013 Jason&Cory. All rights reserved.
+//
+
+#import "Day.h"
+#import "Event.h"
+
+
+@implementation Day
+
+@dynamic date;
+@dynamic name;
+@dynamic event;
+
+@end
---
+++ Event.m
@@ -0,0 +1,21 @@
+//
+//  Event.m
+//  NSO-App
+//
+//  Created by CS212 on 13-11-20.
+//  Copyright (c) 2013 Jason&Cory. All rights reserved.
+//
+
+#import "Event.h"
+#import "Day.h"
+
+
+@implementation Event
+
+@dynamic eventDescription;
+@dynamic eventLocation;
+@dynamic eventTime;
+@dynamic eventTitle;
+@dynamic day;
+
+@end

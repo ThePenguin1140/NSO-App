3031651f5304aa8df99ca18293668ac1ed03a199
C2 <corytclow@gmail.com>
2013-11-21 11:10:39 -0400

    ContactList Impletmented into project
    -view contact
    -add contact
    -tab icon

--- NSO-App/AddressViewController.m
+++ NSO-App/AddressViewController.m
@@ -7,8 +7,21 @@
 //
 
 #import "AddressViewController.h"
+#import <AddressBook/AddressBook.h>
+#import <AddressBookUI/AddressBookUI.h>
+enum TableRowSelected
+{
+	kUIDisplayPickerRow = 0,
+	kUICreateNewContactRow,
+	kUIDisplayContactRow,
+};
 
-@interface AddressViewController ()
+@interface AddressViewController () < ABPeoplePickerNavigationControllerDelegate,ABPersonViewControllerDelegate,
+ABNewPersonViewControllerDelegate>
+
+@property (nonatomic, assign) ABAddressBookRef addressBook;
+@property (nonatomic, strong) NSMutableArray *menuArray;
+
 
 @end
 
@@ -18,7 +31,7 @@
 {
     self = [super initWithStyle:style];
     if (self) {
-        // Custom initialization
+        self.title = @"Contact List";
     }
     return self;
 }
@@ -27,95 +40,125 @@
 {
     [super viewDidLoad];
 
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    _addressBook = ABAddressBookCreateWithOptions(NULL, NULL);
+    self.menuArray = [[NSMutableArray alloc] initWithCapacity:0];
+    
+    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"Menu" ofType:@"plist"];
+	self.menuArray = [NSMutableArray arrayWithContentsOfFile:plistPath];
+    [self.tableView reloadData];
 }
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-#pragma mark - Table view data source
-
+#pragma mark Table view methods
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+	return [self.menuArray count];
 }
 
+// Customize the number of rows in the table view.
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return 1;
 }
 
+// Customize the appearance of table view cells.
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    UITableViewCell *aCell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
-    // Configure the cell...
+    aCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    aCell.textLabel.textAlignment = NSTextAlignmentCenter;
     
-    return cell;
+	
+	aCell.textLabel.text = [[self.menuArray objectAtIndex:indexPath.section] valueForKey:@"title"];
+	return aCell;
 }
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-#pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
+	switch (indexPath.section)
+	{
+		case kUIDisplayPickerRow:
+			[self showPeoplePickerController];
+			break;
+		case kUICreateNewContactRow:
+			[self showNewPersonViewController];
+			break;
+            
+		default:
+			[self showPeoplePickerController];
+			break;
+	}
 }
+
+#pragma mark Show all contacts
+// Called when users tap "Display Picker" in the application. Displays a list of contacts and allows users to select a contact from that list.
+// The application only shows the phone, email, and birthdate information of the selected contact.
+-(void)showPeoplePickerController
+{
+	ABPeoplePickerNavigationController *picker = [[ABPeoplePickerNavigationController alloc] init];
+    picker.peoplePickerDelegate = self;
+	// Display only a person's phone, email, and birthdate
+	NSArray *displayedItems = [NSArray arrayWithObjects:[NSNumber numberWithInt:kABPersonPhoneProperty],
+                               [NSNumber numberWithInt:kABPersonEmailProperty],
+                               [NSNumber numberWithInt:kABPersonBirthdayProperty], nil];
+	
+	
+	picker.displayedProperties = displayedItems;
+	// Show the picker
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+
+#pragma mark Create a new person
+// Called when users tap "Create New Contact" in the application. Allows users to create a new contact.
+-(void)showNewPersonViewController
+{
+	ABNewPersonViewController *picker = [[ABNewPersonViewController alloc] init];
+	picker.newPersonViewDelegate = self;
+	
+	UINavigationController *navigation = [[UINavigationController alloc] initWithRootViewController:picker];
+    [self presentViewController:navigation animated:YES completion:nil];
+}
+
+
+
+#pragma mark ABPeoplePickerNavigationControllerDelegate methods
+// Displays the information of a selected person
+- (BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker shouldContinueAfterSelectingPerson:(ABRecordRef)person
+{
+	return YES;
+}
+
+// Dismisses the people picker and shows the application when users tap Cancel.
+- (void)peoplePickerNavigationControllerDidCancel:(ABPeoplePickerNavigationController *)peoplePicker;
+{
+	[self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+
+
+#pragma mark ABNewPersonViewControllerDelegate methods
+// Dismisses the new-person view controller.
+- (void)newPersonViewController:(ABNewPersonViewController *)newPersonViewController didCompleteWithNewPerson:(ABRecordRef)person
+{
+	[self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+
+#pragma mark Memory management
+- (void)didReceiveMemoryWarning
+{
+    // Release the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+}
+
+- (void)dealloc
+{
+    if(_addressBook)
+    {
+        CFRelease(_addressBook);
+    }
+}
+
 
 @end
